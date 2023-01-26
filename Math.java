ad1950293144ea598ba6e2cbda3b22f35c69359f
alesikaa <oleseacucerean@gmail.com>
2023-01-25 22:15:11 -0500

    Return  Methods

---
+++ Math.java
@@ -0,0 +1,17 @@
+package Class14;
+
+public class Math {
+    //write a method that takes 2 int numbers add them and show the results or
+    void add( int num1, int num2){
+        System.out.println(num1+num2);
+    }
+    //create a method that takes 3 int numbers and multiply them show
+    //the results on console
+
+    void mul(int num1, int num2, int num3){
+        System.out.println(num1*num2*num3);
+}
+int sub (int num1, int num2){
+        return num1-num2;
+}
+}
---
+++ MethodsDemo2.java
@@ -0,0 +1,22 @@
+package Class14;
+import java.util.Scanner;
+public class MethodsDemo2 {
+    // defining a method
+void printHello(){
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+}
+void printHelloManyTimes(int times){
+   for (int i=0; i<times;i++){
+       System.out.println("Hello World");
+   }
+}
+void printManyTimes(int times, String word){
+    for ( int i=0; i<times;i++){
+        System.out.println(word);
+    }
+}
+
+    }
+
